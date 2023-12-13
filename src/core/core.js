9f369a06a16b5837c0839032ba35e474e6c9a522
hachirog <hachiro@grape80.com>
2023-12-13 23:54:40 +0900

    Create files.

---
+++ src/core/core.js
@@ -0,0 +1,38 @@
+// @ts-check
+
+/**
+* Creates an instance of sesame.
+*
+* @constructor
+* @param {string} parameter - The parameter used to select elements.
+* Switches the method to retrieve the element based on the initial letter of the parameter:
+*   - '#' : Uses document.getElementById()
+*   - '.' : Uses document.getElementsByClassName()
+*   - '*' : Uses document.querySelectorAll()
+* @returns {sesame} - The sesame instance.
+*/
+const sesame = function (parameter) {
+    // Create an instance without the new keyword.
+    if (!(this instanceof sesame)) {
+        return new sesame(parameter);
+    }
+
+    if (typeof parameter === 'string') {
+        if (parameter.startsWith('#')) {
+            this.elements = [document.getElementById(parameter.slice(1))];
+        } else if (parameter.startsWith('.')) {
+            this.elements = Array.from(document.getElementsByClassName(parameter.slice(1)));
+        } else if (parameter.startsWith('*')) {
+            this.elements = Array.from(document.querySelectorAll(parameter.slice(1)));
+        }
+    }
+
+    return this;
+}
+
+/**
+ * @type {Array<any>}
+ */
+sesame.prototype.elements = [];
+
+export { sesame };
